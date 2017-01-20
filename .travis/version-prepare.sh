210081d376b7bd4dd77ac1db1272dadc4be703c2
Marco Trevisan (Treviño) <mail@3v1n0.net>
2017-01-20 04:03:14 +0100

    travis: check for version before building

--- .travis/version-prepare.sh
+++ .travis/version-prepare.sh
@@ -4,10 +4,12 @@ REMMINA_GIT="https://github.com/FreeRDP/Remmina.git"
 
 last_tag=$(git ls-remote --tags $REMMINA_GIT | cut -f2 | cut -f3 -d/ | sort -Vr | head -n1)
 last_version=$(basename $last_tag | sed s/^v//)
+channel="edge"
 
 if [ "$BUILD_TARGET" ==  "release" ]; then
   git_ref=$last_tag
   snap_version=$last_version
+  channel="stable"
 elif [ -n "$BUILD_TARGET" ]; then
   git_commit=$(git ls-remote --heads $REMMINA_GIT $BUILD_TARGET | cut -f1)
   git_ref="refs/heads/$BUILD_TARGET"
@@ -23,9 +25,24 @@ if [ -n "$ARCH" ]; then
   - $ARCH" >> snapcraft.yaml
 fi
 
-echo -n "$snap_version" > .snapversion
-
 echo "Snap version is $snap_version"
 echo " - Building from git ref $git_ref"
+
+if [ -z "$SNAP_FORCE_DEPLOY" ]; then
+  arch="amd64"
+  if [ -n "$ARCH" ]; then
+    arch=$ARCH
+  fi
+
+  snap_history=$(docker exec -i builder snapcraft history remmina --arch=$arch | grep $channel)
+  if (echo "$snap_history" | grep -Fq "$snap_version"); then
+    echo "============"
+    echo "Snap version $snap_version is already available in $channel:"
+    echo "  $snap_history" | grep $snap_version | grep $channel | head -n1
+    echo "Skipping deploy, use $SNAP_FORCE_DEPLOY to force"
+    touch .snap_skip_deploy
+  fi
+fi
+
 echo "============"
 cat snapcraft.yaml
